edc7f8e384e2d23e1ab6cfc33019b6b378482046
egor-romanov <egor.romanov@gmail.com>
2023-06-08 01:43:20 +0300

    fix formatting issues

--- app/main.py
+++ app/main.py
@@ -6,23 +6,28 @@ app = FastAPI()
 # list to persist items
 items = []
 
+
 class Item(BaseModel):
     name: str
     price: float
     is_offer: bool = None
-    
+
+
 @app.get("/hello")
 def hello():
-  return {"Hello": "World"}
+    return {"Hello": "World"}
+
 
 @app.get("/item/{name}")
 def get_item(name: str):
-  return {"name": name, "price": 19.99, "is_offer": True}
+    return {"name": name, "price": 19.99, "is_offer": True}
+
 
 @app.post("/item")
 def create_item(item: Item):
-  items.append(item)
-  
+    items.append(item)
+
+
 @app.get("/items")
 def get_items():
-  return items
+    return items
