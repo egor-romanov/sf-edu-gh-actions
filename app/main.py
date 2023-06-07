ea462955fe6387c203d75d0c2813f49103728d8f
egor-romanov <egor.romanov@gmail.com>
2023-06-08 01:25:16 +0300

    init commit; add dummy project

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# list to persist items
+items = []
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
+    
+@app.get("/hello")
+def hello():
+  return {"Hello": "World"}
+
+@app.get("/item/{name}")
+def get_item(name: str):
+  return {"name": name, "price": 19.99, "is_offer": True}
+
+@app.post("/item")
+def create_item(item: Item):
+  items.append(item)
+  
+@app.get("/items")
+def get_items():
+  return items
---
+++ tests/test_main.py
@@ -0,0 +1,17 @@
+from starlette.testclient import TestClient
+
+def test_hello_endpoint(testclient: TestClient):
+  response = testclient.get("/hello")
+  assert response.status_code == 200
+  assert response.json() == {"Hello": "World"}
+  
+def test_get_item(testclient: TestClient):
+  response = testclient.get("/item/foo")
+  assert response.status_code == 200
+  assert response.json() == {"name": "foo", "price": 19.99, "is_offer": True}
+  
+def test_add_item(testclient: TestClient):
+  response = testclient.post("/item", json={"name": "foo", "price": 19.99, "is_offer": True})
+  assert response.status_code == 200
+  response_list = testclient.get("/items")
+  assert response_list.json() == [{"name": "foo", "price": 19.99, "is_offer": True}]
